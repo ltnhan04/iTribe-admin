bb1323289fbbd4b1e154d70cffae3beb7bc8bca5
Bao <huynhbao103@gmail.com>
2024-11-19 23:58:09 +0700

    feat orrder detail

--- src/api/services/order/orderApi.ts
+++ src/api/services/order/orderApi.ts
@@ -3,13 +3,10 @@ import { axiosInstance } from "../../../config/axiosInstance";
 export const getOrders = async () => {
   return await axiosInstance.get(`/api/admin/orders`);
 };
-
+export const getOrderDetail = async (orderId: string) => {
+  return await axiosInstance.get(`/api/admin/orders/${orderId}`);
+};
 export const updateOrderStatus = async (orderId: string, status: string) => {
   return await axiosInstance.put(`/api/admin/orders/${orderId}`, { status });
 };
-
-export const deleteOrder = async (orderId: string) => {
-  return await axiosInstance.delete(`/api/admin/orders/${orderId}`);
-};
-
 export const paginatedOrders = async () => {};
